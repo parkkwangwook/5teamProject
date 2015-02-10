324f117327dbd75fd6601232f6bff293936ee95b
Ugee <Ugee@Ugee-PC>
2015-02-10 10:44:14 +0900

    알바,사장 마이페이지 (비밀번호수정,탈퇴빼고 페이지연결끝)

--- WorkingTimeTable/src/main/java/com/bmj/controller/UsersController.java
+++ WorkingTimeTable/src/main/java/com/bmj/controller/UsersController.java
@@ -1,7 +1,5 @@
 package com.bmj.controller;
 
-
-
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 
@@ -31,145 +29,163 @@ import com.bmj.service.CompanyPersonService;
 import com.bmj.service.UsersService;
 
 @Controller
-/*@RequestMapping("/webProject")*/
+/* @RequestMapping("/webProject") */
 @SessionAttributes("addUser")
 public class UsersController {
 	private static final Logger logger;
 	static {
 		logger = LoggerFactory.getLogger(UsersController.class);
 	}
-	
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, true));
 	}
-	
+
 	@Autowired
 	UsersService service;
 	@Autowired
 	CompanyPersonService cpService;
-	
-	@RequestMapping(value="/count", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/count", method = RequestMethod.GET)
 	public String getAllDeptList(Model model) {
 		Integer result = service.getUsersCount();
 		model.addAttribute("message", result);
 		return "showMessage";
 	}
-	
-	@RequestMapping(value="/webProject/main")
+
+	@RequestMapping(value = "/webProject/main")
 	public String connectionSite() {
 		return "/webProject/intro";
 	}
 
-	@RequestMapping(value="/webProject/join")
+	@RequestMapping(value = "/webProject/join")
 	public String joinGo(Model model) {
 		model.addAttribute("addUser", new Users());
 		return "/webProject/joinForm";
 	}
-	@RequestMapping(value="/webProject/login")											
+
+	@RequestMapping(value = "/webProject/login")
 	public String loginGo() {
 		return "/webProject/login";
 	}
-	
-	@RequestMapping(value="/webProject/join", method = RequestMethod.POST )
-	public String joinSite(@ModelAttribute("addUser") Users user, BindingResult result) {
+
+	@RequestMapping(value = "/webProject/join", method = RequestMethod.POST)
+	public String joinSite(@ModelAttribute("addUser") Users user,
+			BindingResult result) {
 		logger.trace("수업2 : aaaaaaaaaaaaaaaaaaaaaaa" + user);
 		service.insertUser(user);
 		return "/webProject/login";
 	}
-	
-///////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	@RequestMapping(value="/index")											//header join메뉴 눌렀을 때
+
+	// /////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@RequestMapping(value = "/index")
+	// header join메뉴 눌렀을 때
 	public String indexGo() {
 		return "/main/index";
 	}
-	@RequestMapping(value="/join")											//header join메뉴 눌렀을 때
+
+	@RequestMapping(value = "/join")
+	// header join메뉴 눌렀을 때
 	public String joinGGo() {
 		return "/join/join";
 	}
-	@RequestMapping(value="/login")											//header login메뉴 눌렀을 때
+
+	@RequestMapping(value = "/login")
+	// header login메뉴 눌렀을 때
 	public String loginGGo() {
 		return "/login/login";
 	}
-	@RequestMapping(value="/contact")										//header contact메뉴 눌렀을 때
+
+	@RequestMapping(value = "/contact")
+	// header contact메뉴 눌렀을 때
 	public String contactGo() {
 		return "/contact/contact";
 	}
-	
-	////////////////사장 마이페이지
-	@RequestMapping(value="/mypage_employer")								//사장 mypage 메뉴 눌렀을 때
+
+	// //////////////사장 마이페이지
+	@RequestMapping(value = "/mypage_employer")
+	// 사장 mypage 메뉴 눌렀을 때
 	public String mypageEmployerGo() {
 		return "/mypage/employer/mypage";
 	}
-	
-	@RequestMapping(value="/myCompany")										//사장 mypage 메뉴에서 Store(매장관리)
+
+	@RequestMapping(value = "/myCompany")
+	// 사장 mypage 메뉴에서 Store(매장관리)
 	public String mypageMyCompanyGo(Model model, HttpSession session) {
-		Users owner = (Users)session.getAttribute("addUser");
+		Users owner = (Users) session.getAttribute("addUser");
 		CompanyPerson companyCode = null;
 		String viewPath = "";
-		
+
 		companyCode = cpService.selectCompanyCodeByUserId(owner.getUserId());
-		
-		if(companyCode == null ){
-			//등록된 회사 코드가 없는 것
+
+		if (companyCode == null) {
+			// 등록된 회사 코드가 없는 것
 			model.addAttribute("addCmp", new Company());
 			viewPath = "/mypage/employer/registerCompany";
-		}else {
-			//등록한 회사가 있는 것 
+		} else {
+			// 등록한 회사가 있는 것
 			viewPath = "/mypage/employer/myCompany";
 		}
-			
+
 		return viewPath;
 	}
-	
-/*	@RequestMapping(value="/myCompany", method = RequestMethod.POST)	
-	public String mypageMyCompanyGo(Model model, HttpSession session) {
-		
 
-	}*/
-	
-	
-	
-	
-	@RequestMapping(value="/wage")											//사장 mypage 메뉴에서 Wage(알바생들 줄 급여관리)
+	@RequestMapping(value = "/wage")
+	// 사장 mypage 메뉴에서 Wage(알바생들 줄 급여관리)
 	public String mypageWageGo() {
 		return "/mypage/employer/wage";
 	}
-	@RequestMapping(value="/staff")											//사장 mypage 메뉴에서 Staff(알바생관리)
+
+	@RequestMapping(value = "/staff")
+	// 사장 mypage 메뉴에서 Staff(알바생관리)
 	public String mypageStaffGo() {
 		return "/mypage/employer/staff";
 	}
-	@RequestMapping(value="/alerts_employer")										//사장 mypage 메뉴에서 Alerts(쪽지관리)
+
+	@RequestMapping(value = "/alerts_employer")
+	// 사장 mypage 메뉴에서 Alerts(쪽지관리)
 	public String mypagAlertsEmployerGo() {
 		return "/mypage/employer/alerts";
 	}
-	
-	////////////////알바 마이페이지
-	@RequestMapping(value="/mypage_employee")								//알바 mypage 메뉴 눌렀을 때
+
+	// //////////////알바 마이페이지
+	@RequestMapping(value = "/mypage_employee")
+	// 알바 mypage 메뉴 눌렀을 때
 	public String mypageEmployeeGo() {
 		return "/mypage/employee/mypage";
 	}
-	@RequestMapping(value="/myJob")											//알바 mypage 메뉴에서 직업관리
+
+	@RequestMapping(value = "/myJob")
+	// 알바 mypage 메뉴에서 직업관리
 	public String mypageMyJobGo() {
 		return "/mypage/employee/myJob";
 	}
-	@RequestMapping(value="/salary")										//알바 mypage 메뉴에서 Salary(월급관리)
+
+	@RequestMapping(value = "/salary")
+	// 알바 mypage 메뉴에서 Salary(월급관리)
 	public String mypageSalaryGo() {
 		return "/mypage/employee/salary";
 	}
-	@RequestMapping(value="/alerts_employee")								//알바 mypage 메뉴에서 Alerts(쪽지관리)
+
+	@RequestMapping(value = "/alerts_employee")
+	// 알바 mypage 메뉴에서 Alerts(쪽지관리)
 	public String mypagAlertsEmployeeGo() {
 		return "/mypage/employee/alerts";
 	}
-	
-	
-	@RequestMapping(value="/join", method = RequestMethod.POST )			//join페이지에서 가입 성공했을 때
-	public String joinSuccess(@RequestParam String userId, @RequestParam String password, @RequestParam String password2, 
-			@RequestParam String userName, @RequestParam String tel, @RequestParam String email, @RequestParam String birth,
-			@RequestParam String grade, @RequestParam String question, @RequestParam String answer, Model model) {
-		
+
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	// join페이지에서 가입 성공했을 때
+	public String joinSuccess(@RequestParam String userId,
+			@RequestParam String password, @RequestParam String password2,
+			@RequestParam String userName, @RequestParam String tel,
+			@RequestParam String email, @RequestParam String birth,
+			@RequestParam String grade, @RequestParam String question,
+			@RequestParam String answer, Model model) {
+
 		Users loginUser = new Users();
 		loginUser.setUserId(userId);
 		loginUser.setPassword(password);
@@ -181,37 +197,60 @@ public class UsersController {
 		loginUser.setGrade(grade);
 		loginUser.setQuestion(question);
 		loginUser.setAnswer(answer);
-		
+
 		service.insertUser(loginUser);
-		model.addAttribute("addUser",loginUser);
-		
+		model.addAttribute("addUser", loginUser);
+
 		return "/join/welcome";
 	}
-	
-	@RequestMapping(value="/login", method = RequestMethod.POST )			//login성공하면 메인화면으로 
-	public String loginSuccess(@RequestParam String userId, @RequestParam String password, Model model) {
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	// login성공하면 메인화면으로
+	public String loginSuccess(@RequestParam String userId,
+			@RequestParam String password, Model model) {
 		Users loginUser = new Users();
-		loginUser.setUserId(userId);	
+		loginUser.setUserId(userId);
 		loginUser.setPassword(password);
 		logger.trace("수업 : " + loginUser);
-		//logger.trace("수업 : " + loginUser);
+		// logger.trace("수업 : " + loginUser);
 		loginUser = service.loginUser(loginUser);
 		model.addAttribute("addUser", loginUser);
 		return "/main/index";
 	}
-	
-	//로그아웃 수정..중
-	@RequestMapping(value="/logout")										//header logout메뉴 눌렀을 때
+
+	// 로그아웃 수정..중
+	@RequestMapping(value = "/logout")
+	// header logout메뉴 눌렀을 때
 	public String logoutGo(SessionStatus sessionStatus) {
-		/*sessionStatus.setComplete();*/
-		/*session.invalidate();*/
+		/* sessionStatus.setComplete(); */
+		/* session.invalidate(); */
 		sessionStatus.setComplete();
-		/*logger.trace("수업 : " + addUser.userId);*/
-		/*session = (HttpSession) sessionStatus;
-		session.invalidate();*/
+		/* logger.trace("수업 : " + addUser.userId); */
+		/*
+		 * session = (HttpSession) sessionStatus; session.invalidate();
+		 */
 		return "/main/logout";
 	}
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+	//////////////사장 시간표 메뉴 
+	@RequestMapping(value = "/registerSchedule")
+	public String registerScheduleGo() {									//시간표 - 시간표 등록
+		return "/schedule/employer/registerSchedule";
+	}
+	@RequestMapping(value = "/modifySchedule")
+	public String modifyScheduleGo() {										//시간표 - 시간표 수정
+		return "/schedule/employer/modifySchedule";
+	}
+	@RequestMapping(value = "/allSchedule")
+	public String allScheduleGo() {											//시간표 - 전체시간표 조회로 이동
+		return "/schedule/employer/allSchedule";
+	}
 	
-	
+	//////////////알바 시간표 메뉴 
+	@RequestMapping(value = "/mySchedule")
+	public String myScheduleGo() {											//시간표 - 내시간표 조회(직장별)
+		return "/schedule/employee/mySchedule";
+	}
 }
--- WorkingTimeTable/src/main/webapp/WEB-INF/view/main/index.jsp
+++ WorkingTimeTable/src/main/webapp/WEB-INF/view/main/index.jsp
@@ -8,44 +8,55 @@
 
 <html>
 <head>
-		<meta charset="utf-8">
-		<title>Welcome</title>
-		<link rel="stylesheet" href="<%=request.getContextPath()%>/css/reset.css" type="text/css" media="screen">
-    	<link rel="stylesheet" href="<%=request.getContextPath()%>/css/style.css" type="text/css" media="screen">  
-		<link rel="stylesheet" href="<%=request.getContextPath()%>/css/zerogrid.css" type="text/css" media="all">
-		<link rel="stylesheet" href="<%=request.getContextPath()%>/css/responsive.css" type="text/css" media="all">
-	
-		<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
-    	<script src="js/cufon-yui.js" type="text/javascript"></script>
-    	<script src="js/cufon-replace.js" type="text/javascript"></script>
-    	<script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js" type="text/javascript"></script>
-    	<script src="js/Kozuka_Gothic_Pro_OpenType_700.font.js" type="text/javascript"></script>
-    	<script src="js/Kozuka_Gothic_Pro_OpenType_900.font.js" type="text/javascript"></script>
-    	<script src="js/FF-cash.js" type="text/javascript"></script>     
-   		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-		<script type="text/javascript" src="js/tms-0.3.js"></script>
-		<script type="text/javascript" src="js/tms_presets.js"></script>
-    	<script type="text/javascript" src="js/easyTooltip.js"></script> 
-    	<script type="text/javascript" src="js/script.js"></script>
-		<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
-		
-	</head> 
+<meta charset="utf-8">
+<title>Welcome</title>
+<link rel="stylesheet"
+	href="<%=request.getContextPath()%>/css/reset.css" type="text/css"
+	media="screen">
+<link rel="stylesheet"
+	href="<%=request.getContextPath()%>/css/style.css" type="text/css"
+	media="screen">
+<link rel="stylesheet"
+	href="<%=request.getContextPath()%>/css/zerogrid.css" type="text/css"
+	media="all">
+<link rel="stylesheet"
+	href="<%=request.getContextPath()%>/css/responsive.css" type="text/css"
+	media="all">
+
+<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Kozuka_Gothic_Pro_OpenType_300.font.js"
+	type="text/javascript"></script>
+<script src="js/Kozuka_Gothic_Pro_OpenType_700.font.js"
+	type="text/javascript"></script>
+<script src="js/Kozuka_Gothic_Pro_OpenType_900.font.js"
+	type="text/javascript"></script>
+<script src="js/FF-cash.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/tms-0.3.js"></script>
+<script type="text/javascript" src="js/tms_presets.js"></script>
+<script type="text/javascript" src="js/easyTooltip.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+
+</head>
 <body id="page1">
-	
-    <jsp:include page="/WEB-INF/view/main/header.jsp" />
-    
+
+	<jsp:include page="/WEB-INF/view/main/header.jsp" />
+
 	<!--==============================advertisement================================-->
-	
+
 	<div class="slider-wrapper">
 		<div class="slider">
 			<ul class="items">
-				<li><img src="images/slider-bg1.png" alt=""> <div class="banner">
-                        	<strong>need <strong>fresh ideas?</strong></strong>
-                            <em>Let’s create your company’s growth strategy together!</em>
-                            <span class="button">
-                            	<a href="#"><strong>Read More</strong></a>
-                            </span>
-                        </div></li>
+				<li><img src="images/slider-bg1.png" alt="">
+					<div class="banner">
+						<strong>need <strong>fresh ideas?</strong></strong> <em>Let’s
+							create your company’s growth strategy together!</em> <span
+							class="button"> <a href="#"><strong>Read More</strong></a>
+						</span>
+					</div></li>
 				<li><img src="images/slider-bg2.png" alt=""> <!-- <div class="banner">
                         	<strong>good <strong>solutions!</strong></strong>
                             <em>Make your company a world wide known name with us!</em>
@@ -63,17 +74,26 @@
 			</ul>
 		</div>
 	</div>
-	
+
 	<!--==============================main menu================================-->
 
 	<div class="mainmenubg">
 		<div class="main zerogrid">
 			<div class="row">
 				<article class="col-1-3">
-					<a href="mypage.jsp"><img src="images/attendance.png" alt="출석체크"></a>
+					<a href="mypage.jsp"><img src="images/attendance.png"
+						alt="출석체크"></a>
 				</article>
 				<article class="col-1-3">
-					<a href="timetable.jsp"><img src="images/timetable.png" alt="근무표조회"></a>
+
+					<c:if test="${addUser.grade=='사장' }">
+						<c:url value="/allSchedule" var="url" />
+					</c:if>
+					<c:if test="${addUser.grade=='알바' }">
+						<c:url value="/mySchedule" var="url" />
+					</c:if>
+					<a href="${url }"><img
+						src="images/timetable.png" alt="근무표조회"></a>
 				</article>
 				<article class="col-1-3">
 					<a href="board.jsp"><img src="images/board.png" alt="자유게시판"></a>
@@ -81,9 +101,9 @@
 			</div>
 		</div>
 	</div>
-	
-    <!--==============================2 section================================-->
-   <!--  <div class="mainmenubg"> -->
+
+	<!--==============================2 section================================-->
+	<!--  <div class="mainmenubg"> -->
 	<section id="content">
 		<div class="main zerogrid">
 			<div class="row">
@@ -126,26 +146,27 @@
 							</div>
 							<div class="clear"></div>
 						</div>
-						<span class="button-2"> <a href="board.jsp"><strong>Go Freeboard</strong></a>
+						<span class="button-2"> <a href="board.jsp"><strong>Go
+									Freeboard</strong></a>
 						</span>
 					</div>
 				</article>
 				<article class="col-1-2">
 					<div class="wrap-col">
 						<h3>Today's Weather</h3>
-						
-						<span class="button-2"><a href="http://www.kma.go.kr" target="_blank"><strong>Connect to KMA</strong></a>
-						</span>
+
+						<span class="button-2"><a href="http://www.kma.go.kr"
+							target="_blank"><strong>Connect to KMA</strong></a> </span>
 					</div>
 				</article>
-				
+
 			</div>
 		</div>
 	</section>
 	<!-- </div> -->
-	
-	
-    <jsp:include page="/WEB-INF/view/main/footer.jsp" />
+
+
+	<jsp:include page="/WEB-INF/view/main/footer.jsp" />
 
 
 	<script type="text/javascript">
@@ -164,7 +185,7 @@
 				waitBannerAnimation : false
 			})
 		})
-	</script> 
+	</script>
 </body>
 
 </html>

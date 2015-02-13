60380a3918fd9ab5af045814a10d9255bb189f56
ce401-19 <ce401-19@ce401-19-HP>
2015-02-13 13:28:32 +0900

    13:28

--- WorkingTimeTable/java/com/bmj/controller/ChartController.java
+++ WorkingTimeTable/java/com/bmj/controller/ChartController.java
@@ -1,5 +1,7 @@
 package com.bmj.controller;
 
+import java.util.Calendar;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,7 +16,13 @@ public class ChartController {
 	
 	@RequestMapping(value = "/chart")
 	public String goChartAlba() {
-		
+		Calendar c1 = Calendar.getInstance();
+		logger.trace("수업 : Chart~~~~~~~~~~~~~~!");
+		// 1. session에서 내 정보를 가져오겠지...?
+		// 2. 내 아이디로 나의 직원 아이디를 가져오고!
+		// 3. 직원 아이디로 근무표를 조회한다.
+		// 4. 
+		int currentMonth;
 		
 		
 		return "chart/albamode";
